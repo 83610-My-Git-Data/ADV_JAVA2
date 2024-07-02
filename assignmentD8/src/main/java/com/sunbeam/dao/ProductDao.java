e84f9f4c80575ee20d0a69a9e8674f142562bc4b
Aniket Nikam <100779784+aniketnikam4687@users.noreply.github.com>
2024-07-02 15:55:59 +0530

    assignment 5 uploadad

--- assignmentD8/src/main/java/com/sunbeam/dao/ProductDao.java
+++
@@ -1,8 +0,0 @@
-package com.sunbeam.dao;
-
-import com.sunbeam.entities.Product;
-
-public interface ProductDao {
-    String addProduct(Product product);
-    Product getProductById(Long productId);
-}
